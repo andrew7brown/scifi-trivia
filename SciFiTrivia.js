baa4e1653901a2cdda9436cc075d0c59cfe25f09
Andrew T. Brown <andrew7brown@gmail.com>
2016-04-10 20:16:36 -0600

    Comment update

--- SciFiTrivia.js
+++ SciFiTrivia.js
@@ -1,17 +1,6 @@
 /**
- * Created by Andrew on 4/10/2016.
- */
-/**
- Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
- Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
- http://aws.amazon.com/apache2.0/
-
- or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
- */
-
-/**
+ * Created by Andrew T. Brown on 4/10/2016.
+ *
  * This is a Science Fiction Trivia game using the Alexa Skill kit. The skill
  * supports 1 player at a time, and does not support games across sessions.
  * This was based on the Reindeer Games sample provided by Amazon.
